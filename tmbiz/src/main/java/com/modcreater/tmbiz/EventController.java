bb6a9c72da1c63517a7d2ca859c974f72f662e7f
[username] <[email]>
2019-04-29 16:17:55 +0800

    null

--- tmbiz/src/main/java/com/modcreater/tmbiz/EventController.java
+++ tmbiz/src/main/java/com/modcreater/tmbiz/EventController.java
@@ -5,6 +5,7 @@ import com.modcreater.tmbeans.pojo.SingleEvent;
 import com.modcreater.tmbiz.service.EventService;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
@@ -24,7 +25,7 @@ public class EventController {
     @Resource
     private EventService eventService;
 
-    @RequestMapping(value = "upl")
+    @RequestMapping(value = "upl" ,method = RequestMethod.POST)
     public Dto uploading(@RequestBody SingleEvent singleEvent){
         return eventService.addNewEvents(singleEvent);
     }
