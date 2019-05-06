8c2450f57535e22332791e80a592e36d2648f323
[username] <[email]>
2019-05-06 10:32:39 +0800

    null

--- tmbiz/src/main/java/com/modcreater/tmbiz/service/impl/EventServiceImpl.java
+++ tmbiz/src/main/java/com/modcreater/tmbiz/service/impl/EventServiceImpl.java
@@ -36,25 +36,8 @@ public class EventServiceImpl implements EventService {
     @Override
     public Dto addNewEvents(UploadingEventVo uploadingEventVo) {
         if (!ObjectUtils.isEmpty(uploadingEventVo)) {
-            SingleEvent singleEvent = new SingleEvent();
-            singleEvent.setEventid(uploadingEventVo.getEventid());
-            singleEvent.setUserid(uploadingEventVo.getUserid());
-            singleEvent.setEventname(uploadingEventVo.getEventname());
-            singleEvent.setStarttime(uploadingEventVo.getStarttime());
-            singleEvent.setEndtime(uploadingEventVo.getEndtime());
-            singleEvent.setAddress(uploadingEventVo.getAddress());
-            singleEvent.setLevel(uploadingEventVo.getLevel());
-            singleEvent.setFlag(uploadingEventVo.getFlag());
-            singleEvent.setPerson(uploadingEventVo.getPerson());
-            singleEvent.setRemarks(uploadingEventVo.getRemarks());
-            singleEvent.setRepeaTtime(uploadingEventVo.getRepeaTtime());
-            singleEvent.setIsOverdue(uploadingEventVo.getIsOverdue());
-            singleEvent.setRemindTime(uploadingEventVo.getRemindTime());
-            singleEvent.setDay(uploadingEventVo.getDay());
-            singleEvent.setMonth(uploadingEventVo.getMonth());
-            singleEvent.setYear(uploadingEventVo.getYear());
-            singleEvent.setType(uploadingEventVo.getType());
-            if (eventMapper.uploadingEvents(singleEvent) > 0) {
+            SingleEvent singleEvent = get(uploadingEventVo);
+            if (eventMapper.uploadingEvents(singleEvent) > 0 && !ObjectUtils.isEmpty(singleEvent)) {
                 try {
                     if (accountMapper.updateTimestampUnderAccount(singleEvent.getUserid().toString(), DateUtil.dateToStamp(new Date())) > 0) {
                         return DtoUtil.getSuccessDto("事件上传成功", 100000);
@@ -91,25 +74,9 @@ public class EventServiceImpl implements EventService {
     @Override
     public Dto updateEvents(UpdateEventVo updateEventVo) {
         if (!ObjectUtils.isEmpty(updateEventVo)) {
-            SingleEvent singleEvent = new SingleEvent();
-            singleEvent.setEventid(updateEventVo.getEventid());
-            singleEvent.setUserid(updateEventVo.getUserid());
-            singleEvent.setEventname(updateEventVo.getEventname());
-            singleEvent.setStarttime(updateEventVo.getStarttime());
-            singleEvent.setEndtime(updateEventVo.getEndtime());
-            singleEvent.setAddress(updateEventVo.getAddress());
-            singleEvent.setLevel(updateEventVo.getLevel());
-            singleEvent.setFlag(updateEventVo.getFlag());
-            singleEvent.setPerson(updateEventVo.getPerson());
-            singleEvent.setRemarks(updateEventVo.getRemarks());
-            singleEvent.setRepeaTtime(updateEventVo.getRepeaTtime());
-            singleEvent.setIsOverdue(updateEventVo.getIsOverdue());
-            singleEvent.setRemindTime(updateEventVo.getRemindTime());
-            singleEvent.setDay(updateEventVo.getDay());
-            singleEvent.setMonth(updateEventVo.getMonth());
-            singleEvent.setYear(updateEventVo.getYear());
-            singleEvent.setType(updateEventVo.getType());
-            if (eventMapper.alterEventsByUserId(singleEvent) > 0) {
+
+            SingleEvent singleEvent = get(updateEventVo);
+            if (eventMapper.alterEventsByUserId(singleEvent) > 0 && !ObjectUtils.isEmpty(singleEvent)) {
                 try {
                     if (accountMapper.updateTimestampUnderAccount(singleEvent.getUserid().toString(), DateUtil.dateToStamp(new Date())) > 0) {
                         return DtoUtil.getSuccessDto("修改成功", 100000);
@@ -149,93 +116,99 @@ public class EventServiceImpl implements EventService {
         if (synchronousUpdateVo.getDayEventsList().size() <= 0) {
             return DtoUtil.getFalseDto("事件集未获取到", 25002);
         }
-        List<Integer> dayEventIds=new ArrayList<>();
-        for (DayEvents dayEvents:synchronousUpdateVo.getDayEventsList()){
+        List<Integer> dayEventIds = new ArrayList<>();
+        for (DayEvents dayEvents : synchronousUpdateVo.getDayEventsList()) {
             dayEventIds.add(dayEvents.getDayEventId());
         }
         //查询时间段内的事件
-        StringBuffer stringBuffer=null;
-        String year=null;
-        String month=null;
-        String day=null;
-        List<SingleEvent> singleEvents=new ArrayList<>();
-        for (int i = 0; i <dayEventIds.size() ; i++) {
-            stringBuffer=new StringBuffer(dayEventIds.get(i).toString());
+        StringBuffer stringBuffer = null;
+        String year = null;
+        String month = null;
+        String day = null;
+        List<SingleEvent> singleEvents = new ArrayList<>();
+        for (int i = 0; i < dayEventIds.size(); i++) {
+            stringBuffer = new StringBuffer(dayEventIds.get(i).toString());
 //            System.out.println(stringBuffer);
-            year=stringBuffer.substring(0,4);
-            month=stringBuffer.substring(4, 6);
-            day=stringBuffer.substring(6, 8);
-            SingleEvent singleEvent=new SingleEvent();
+            year = stringBuffer.substring(0, 4);
+            month = stringBuffer.substring(4, 6);
+            day = stringBuffer.substring(6, 8);
+            SingleEvent singleEvent = new SingleEvent();
             singleEvent.setYear(Long.parseLong(year));
             singleEvent.setMonth(Long.parseLong(month));
             singleEvent.setDay(Long.parseLong(day));
-            singleEvents=eventMapper.queryEvents(singleEvent);
-            if (ObjectUtils.isEmpty(singleEvents)){
-                return DtoUtil.getFalseDto("该时间段内没有事件",25003);
+            singleEvents = eventMapper.queryEvents(singleEvent);
+            if (ObjectUtils.isEmpty(singleEvents)) {
+                return DtoUtil.getFalseDto("该时间段内没有事件", 25003);
             }
         }
         //删除事件
-        for (int i = 0; i <singleEvents.size() ; i++) {
-            int delResult=eventMapper.withdrawEventsByUserId(singleEvents.get(i));
-            if (delResult<=0){
-                return DtoUtil.getFalseDto("删除失败",25004);
+        for (int i = 0; i < singleEvents.size(); i++) {
+            int delResult = eventMapper.withdrawEventsByUserId(singleEvents.get(i));
+            if (delResult <= 0) {
+                return DtoUtil.getFalseDto("删除失败", 25004);
             }
         }
-       //上传事件
+        //上传事件
         for (int i = 0; i < synchronousUpdateVo.getDayEventsList().size(); i++) {
-            DayEvents dayEvents=synchronousUpdateVo.getDayEventsList().get(i);
+            DayEvents dayEvents = synchronousUpdateVo.getDayEventsList().get(i);
             for (int j = 0; j < dayEvents.getMySingleEventList().size(); j++) {
-                int uplResult=eventMapper.uploadingEvents(dayEvents.getMySingleEventList().get(j));
-                if (uplResult<=0){
-                    return DtoUtil.getFalseDto("上传事件失败",25005);
+                int uplResult = eventMapper.uploadingEvents(dayEvents.getMySingleEventList().get(j));
+                if (uplResult <= 0) {
+                    return DtoUtil.getFalseDto("上传事件失败", 25005);
                 }
             }
         }
         //修改时间戳
         try {
-            int i=accountMapper.updateTimestampUnderAccount(synchronousUpdateVo.getUserId(),DateUtil.dateToStamp(new Date()));
-            if (i<=0){
-                return DtoUtil.getFalseDto("同步数据时修改时间戳失败",25006);
+            int i = accountMapper.updateTimestampUnderAccount(synchronousUpdateVo.getUserId(), DateUtil.dateToStamp(new Date()));
+            if (i <= 0) {
+                return DtoUtil.getFalseDto("同步数据时修改时间戳失败", 25006);
             }
         } catch (ParseException e) {
             e.printStackTrace();
         }
-        return DtoUtil.getSuccessDto("数据同步成功",100000);
+        return DtoUtil.getSuccessDto("数据同步成功", 100000);
     }
 
     @Override
     public Dto contrastTimestamp(ContrastTimestampVo contrastTimestampVo) {
-        if (ObjectUtils.isEmpty(contrastTimestampVo)){
-            return DtoUtil.getFalseDto("时间戳获取失败",24001);
+        if (ObjectUtils.isEmpty(contrastTimestampVo)) {
+            return DtoUtil.getFalseDto("时间戳获取失败", 24001);
         }
-        String time=accountMapper.queryTime(contrastTimestampVo.getUserId());
-        if (StringUtils.isEmpty(time)){
-            return DtoUtil.getFalseDto("查询时间戳失败",200000);
+        String time = accountMapper.queryTime(contrastTimestampVo.getUserId());
+        if (StringUtils.isEmpty(time)) {
+            return DtoUtil.getFalseDto("查询时间戳失败", 200000);
         }
-        Map map=new HashMap();
-        map.put("time",time);
-        return DtoUtil.getSuccesWithDataDto("查询时间戳成功",map,100000);
+        Map map = new HashMap();
+        map.put("time", time);
+        return DtoUtil.getSuccesWithDataDto("查询时间戳成功", map, 100000);
     }
 
-    private static SingleEvent get() {
-        SingleEvent singleEvent = new SingleEvent();
-        singleEvent.setEventid(22L);
-        singleEvent.setUserid(100019L);
-        singleEvent.setEventname("测试事件修改时间戳");
-        singleEvent.setStarttime("1557035000");
-        singleEvent.setEndtime("1557035001");
-        singleEvent.setAddress("测试地址修改时间戳");
-        singleEvent.setLevel(1L);
-        singleEvent.setFlag(1L);
-        singleEvent.setPerson("1,2,3,4,5,6,7");
-        singleEvent.setRemarks("测试修改时间戳");
-        singleEvent.setRepeaTtime("10");
-        singleEvent.setIsOverdue(2L);
-        singleEvent.setRemindTime("1557035000");
-        singleEvent.setDay(4L);
-        singleEvent.setMonth(4L);
-        singleEvent.setYear(2014L);
-        singleEvent.setType(2L);
-        return singleEvent;
+    public static SingleEvent get(Object object) {
+        if (SingleEvent.class.isAssignableFrom(object.getClass())) {
+            SingleEvent singleEvent = new SingleEvent();
+            try {
+                singleEvent.setEventid((Long) object.getClass().getMethod("getEventid").invoke(object));
+                singleEvent.setUserid((Long) object.getClass().getMethod("getUserid").invoke(object));
+                singleEvent.setEventname((String) object.getClass().getMethod("getEventname").invoke(object));
+                singleEvent.setStarttime((String) object.getClass().getMethod("getStarttime").invoke(object));
+                singleEvent.setEndtime((String) object.getClass().getMethod("getEndtime").invoke(object));
+                singleEvent.setAddress((String) object.getClass().getMethod("getAddress").invoke(object));
+                singleEvent.setLevel((Long) object.getClass().getMethod("getLevel").invoke(object));
+                singleEvent.setFlag((Long) object.getClass().getMethod("getFlag").invoke(object));
+                singleEvent.setPerson((String) object.getClass().getMethod("getPerson").invoke(object));
+                singleEvent.setRemarks((String) object.getClass().getMethod("getRemarks").invoke(object));
+                singleEvent.setRepeaTtime((String) object.getClass().getMethod("getRepeaTtime").invoke(object));
+                singleEvent.setIsOverdue((Long) object.getClass().getMethod("getIsOverdue").invoke(object));
+                singleEvent.setRemindTime((String) object.getClass().getMethod("getRemindTime").invoke(object));
+                singleEvent.setDay((Long) object.getClass().getMethod("getDay").invoke(object));
+                singleEvent.setMonth((Long) object.getClass().getMethod("getMonth").invoke(object));
+                singleEvent.setYear((Long) object.getClass().getMethod("getYear").invoke(object));
+                singleEvent.setType((Long) object.getClass().getMethod("getType").invoke(object));
+            } catch (ReflectiveOperationException e) {
+                e.printStackTrace();
+            }
+        }
+        return null;
     }
 }
--- tmbiz/src/test/java/com/modcreater/tmbiz/TmbizApplicationTests.java
+++ tmbiz/src/test/java/com/modcreater/tmbiz/TmbizApplicationTests.java
@@ -1,10 +1,15 @@
 package com.modcreater.tmbiz;
 
+import com.modcreater.tmbeans.pojo.SingleEvent;
+import com.modcreater.tmbeans.vo.UploadingEventVo;
+import com.modcreater.tmbiz.service.impl.EventServiceImpl;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
@@ -14,18 +19,41 @@ import java.util.Date;
 public class TmbizApplicationTests {
 
     @Test
-    public void contextLoads() throws Exception{
+    public void contextLoads() throws Exception {
         Calendar calendar = Calendar.getInstance();
-        Long timeStamp = (calendar.getTimeInMillis())/1000;
+        Long timeStamp = (calendar.getTimeInMillis()) / 1000;
         Date time = new Date(timeStamp);
         System.out.println(time);
         System.out.println(timeStamp);
     }
 
     @Test
-    public void test1(){
+    public void test1() {
         StringBuilder stringBuilder = new StringBuilder("20190506");
-        System.out.println(stringBuilder.substring(6,8));
+        System.out.println(stringBuilder.substring(6, 8));
     }
 
+    @Test
+    public void test2() {
+        UploadingEventVo uploadingEventVo = new UploadingEventVo();
+        uploadingEventVo.setEventid(1L);
+        uploadingEventVo.setUserid(1L);
+        uploadingEventVo.setEventname("测试名称");
+        uploadingEventVo.setStarttime("1557109312");
+        uploadingEventVo.setEndtime("1557109312");
+        uploadingEventVo.setAddress("测试地址");
+        uploadingEventVo.setLevel(1L);
+        uploadingEventVo.setFlag(1L);
+        uploadingEventVo.setPerson("测试人物");
+        uploadingEventVo.setRemarks("测试备注");
+        uploadingEventVo.setRepeaTtime("2");
+        uploadingEventVo.setIsOverdue(0L);
+        uploadingEventVo.setRemindTime("1557109312");
+        uploadingEventVo.setDay(5L);
+        uploadingEventVo.setMonth(5L);
+        uploadingEventVo.setYear(2019L);
+        uploadingEventVo.setType(1L);
+        SingleEvent singleEvent = EventServiceImpl.get(uploadingEventVo);
+        System.out.println(singleEvent);
+    }
 }
