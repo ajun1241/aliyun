3cfad6355a3ef242d0b7dbbb5192b6ecd30ecdda
叫我阿俊就好 <zhoujun1241@gmail.com>
2019-05-07 10:57:06 +0800

    修改时间同步方法

---
+++ tmbeans/src/main/java/com/modcreater/tmbeans/pojo/LoopEvent.java
@@ -0,0 +1,26 @@
+package com.modcreater.tmbeans.pojo;
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+@Data
+public class LoopEvent implements Serializable {
+
+  private long eventId;
+  private long userId;
+  private String eventName;
+  private String startTime;
+  private String endTime;
+  private String address;
+  private long level;
+  private long flag;
+  private String person;
+  private String remarks;
+  private long isOverdue;
+  private String remindTime;
+  private long type;
+  private long day;
+  private long week;
+
+}
