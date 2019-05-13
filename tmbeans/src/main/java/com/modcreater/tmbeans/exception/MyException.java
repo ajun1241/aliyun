391761ba49e6141b41111a924c1eb722e4410aee
Goku_yi <609274756@qq.com>
2019-05-13 17:27:38 +0800

    none

---
+++ tmbeans/src/main/java/com/modcreater/tmbeans/exception/MyException.java
@@ -0,0 +1,16 @@
+package com.modcreater.tmbeans.exception;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ *
+ * @Author: Goku_yi
+ * @Date: 2019-05-13
+ * Time: 16:25
+ */
+public class MyException extends Exception {
+
+    
+
+
+}
--- tmbiz/src/main/java/com/modcreater/tmbiz/controller/ExceptionController.java
+++ tmbiz/src/main/java/com/modcreater/tmbiz/controller/ExceptionController.java
@@ -1,6 +1,13 @@
 package com.modcreater.tmbiz.controller;
 
+import com.modcreater.tmbeans.dto.Dto;
+import com.modcreater.tmbeans.exception.MyException;
+import com.modcreater.tmutils.DtoUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * Created with IntelliJ IDEA.
@@ -12,4 +19,22 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
  */
 @ControllerAdvice
 public class ExceptionController {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(ExceptionController.class);
+
+    @ResponseBody
+    @ExceptionHandler(value = Exception.class)
+    public Dto errorHandler(Exception exception) {
+        LOGGER.error("捕获到全局异常", exception);
+        return DtoUtil.getFalseDto(exception.getClass()+":"+exception.getMessage(), 100);
+    }
+
+    @ResponseBody
+    @ExceptionHandler(value = MyException.class)
+    public Dto myErrorHandler(MyException exception) {
+        LOGGER.error("捕获到自定义异常", exception);
+        return DtoUtil.getFalseDto(exception.getClass()+":"+exception.getMessage(), 101);
+    }
+
+
 }
