4581358370ca49c0ef6b7fccc713604003e7d5f4
Goku_yi <609274756@qq.com>
2019-05-14 14:59:23 +0800

    none

--- tmchat/src/main/java/com/modcreater/tmchat/chat/controller/ChatController.java
+++ tmchat/src/main/java/com/modcreater/tmchat/chat/controller/ChatController.java
@@ -1,6 +1,24 @@
 package com.modcreater.tmchat.chat.controller;
 
+import com.modcreater.tmbeans.dto.Dto;
+import com.modcreater.tmutils.DtoUtil;
+import io.rong.RongCloud;
+import io.rong.messages.TxtMessage;
+import io.rong.methods.message._private.Private;
+import io.rong.methods.message.chatroom.Chatroom;
+import io.rong.methods.message.discussion.Discussion;
+import io.rong.methods.message.group.Group;
+import io.rong.methods.message.history.History;
+import io.rong.methods.message.system.MsgSystem;
+import io.rong.methods.user.User;
+import io.rong.models.Result;
+import io.rong.models.message.PrivateMessage;
+import io.rong.models.message.SystemMessage;
+import io.rong.models.response.ResponseResult;
+import io.rong.models.response.TokenResult;
+import io.rong.models.user.UserModel;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -16,7 +34,74 @@ import org.springframework.web.bind.annotation.RestController;
 public class ChatController {
 
 
+    private static final String appKey = "0vnjpoad03rzz";
+    private static final String appSecret = "BbTOtrRIF5MOA";
+    private static final String api = "http://api-cn.ronghub.com";
+    private static final TxtMessage txtMessage = new TxtMessage("hello", "helloExtra");
 
+    @RequestMapping(value = "solo", method = RequestMethod.POST)
+    public void testSolo() throws Exception{
+        RongCloud rongCloud = RongCloud.getInstance(appKey, appSecret);
+        //自定义 api 地址方式
+        //RongCloud rongCloud = RongCloud.getInstance(appKey, appSecret,api);
 
+        Private Private = rongCloud.message.msgPrivate;
+        MsgSystem system = rongCloud.message.system;
+        Group group = rongCloud.message.group;
+        Chatroom chatroom = rongCloud.message.chatroom;
+        Discussion discussion = rongCloud.message.discussion;
+        History history = rongCloud.message.history;
 
+        /**
+         * API 文档: http://www.rongcloud.cn/docs/server_sdk_api/message/system.html#send
+         *
+         * 发送系统消息
+         *
+         */
+        String[] targetIds = {"2651280140445094444"};
+        SystemMessage systemMessage = new SystemMessage()
+                .setSenderId("usetId")
+                .setTargetId(targetIds)
+                .setObjectName(txtMessage.getType())
+                .setContent(txtMessage)
+                .setPushContent("this is a push")
+                .setPushData("{'pushData':'hello'}")
+                .setIsPersisted(0)
+                .setIsCounted(0)
+                .setContentAvailable(0);
+
+        ResponseResult result = system.send(systemMessage);
+        System.out.println("send system message:  " + result.toString());
+    }
+
+    @RequestMapping(value = "register",method = RequestMethod.POST)
+    public Dto testRegister()throws Exception {
+        RongCloud rongCloud = RongCloud.getInstance(appKey, appSecret);
+        //自定义 api 地址方式
+        // RongCloud rongCloud = RongCloud.getInstance(appKey, appSecret,api);
+        User User = rongCloud.user;
+
+        /**
+         * API 文档: http://www.rongcloud.cn/docs/server_sdk_api/user/user.html#register
+         *
+         * 注册用户，生成用户在融云的唯一身份标识 Token
+         */
+        UserModel user = new UserModel()
+                .setId("userxxd2")
+                .setName("username")
+                .setPortrait("http://www.rongcloud.cn/images/logo.png");
+        TokenResult result = User.register(user);
+        System.out.println("getToken:  " + result.toString());
+
+        /**
+         *
+         * API 文档: http://www.rongcloud.cn/docs/server_sdk_api/user/user.html#refresh
+         *
+         * 刷新用户信息方法
+         */
+        Result refreshResult = User.update(user);
+        System.out.println("refresh:  " + refreshResult.toString());
+
+        return DtoUtil.getSuccesWithDataDto("测试注册",result.toString(),100000);
+    }
 }
---
+++ tmchat/src/main/java/com/modcreater/tmchat/config/SwaggerConfig.java
@@ -0,0 +1,23 @@
+package com.modcreater.tmchat.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+@ComponentScan("com.modcreater.tmchat.*")
+@Component
+public class SwaggerConfig {
+    @Bean
+    public Docket docketid() {
+        Docket docket = new Docket(DocumentationType.SWAGGER_2);
+        docket.apiInfo(new ApiInfo("时间管理", "描述信息", "1.0.0", " ", "233", "授权来自此处", " "));
+        return docket;
+    }
+}
