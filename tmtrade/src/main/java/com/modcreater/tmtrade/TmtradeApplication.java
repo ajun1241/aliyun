322b4bda719d24f3bc82ef87751e3a8e6681718d
Goku_yi <609274756@qq.com>
2019-05-24 17:48:55 +0800

    none

---
+++ tmtrade/src/main/java/com/modcreater/tmtrade/TmtradeApplication.java
@@ -0,0 +1,13 @@
+package com.modcreater.tmtrade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TmtradeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TmtradeApplication.class, args);
+    }
+
+}
