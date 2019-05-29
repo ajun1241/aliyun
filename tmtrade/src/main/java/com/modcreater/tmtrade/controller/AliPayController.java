7aaac3775f53767e3fb0443b97150fa2204f9970
Goku_yi <609274756@qq.com>
2019-05-29 15:35:37 +0800

    none

--- tmtrade/src/main/java/com/modcreater/tmtrade/controller/AliPayController.java
+++ tmtrade/src/main/java/com/modcreater/tmtrade/controller/AliPayController.java
@@ -55,14 +55,15 @@ public class AliPayController {
     private StringRedisTemplate stringRedisTemplate;
 
     public static String APP_ID="2019052065168786";
-    public static String SELLER_ID = "2088531247419714";
+    public static String PID="2088531247419714";
+    public static String SELLER_ID = "huzhou_mod_center@163.com";
     public static String APP_PRIVATE_KEY="MIIEvAIBADANBgkqhkiG9w0BAQEFAASCBKYwggSiAgEAAoIBAQCrOPZAzmxeSSKSdzj5YwUOrSd/GPSReqPfs93isVE6IukwTE6IMMBRhUdeKsaEJFIxCMevjlGTrCnE5pOmz/Ol6rHzwfUlguWywRb/SZkep5OX7Bu5P70H9HqW83uebclMF7P33vImTe8D13RoprSdDXy2VFNMGkts/kyGtpo37uJcpCdZJwNza2j4Tp5NyhVwhecGgF5dnHkkTnmxF5hnyfR9y6wo7PuM5WSGEqj3SMs2y7LzVYqCK6z84c/2sEd4YQS405SYTHSIeQGY4Mhc3u1QswAAXjFdL+9OxkALUBVzUuvryk105nUQT/2sQsaxD0BYDsy8w5bvAZn/s7P9AgMBAAECggEAPEHXqAb9bFEgsZrzmhCW/wtBEyGdMHWQpnGObPEqPON8XVr9aEB/7jCRZTp4kpiVtFv6qnXTj7zPFlRvMg2NGZ728WoY9BKU88uwZDdqg7dF4fld6FveEqafXfBiWOcaKIVO+LSlU4Wp0BhLB2ljtsHAWgpjJi06zijQlbg9neEHPaEnojxiJu00q0a5boy1l0pCwuj2xJGUPwnTMpye3VhOS88MVxUt5HnG6aa+7Y1aZrioJynDsAXR07yFKKe6H3xc9Ymk3DP4u2AH3YYu7uy+F0pijfDp+xe3ucCTBJAB8dJ9FyP8I4j+pQQIy+gTD+mUTy0cmSwAmCDtwYH/AQKBgQDxYAznY3KCDuppxa2A5bvadhioePeJPqZZ/m+fV/XAsT5w1UoSz0SGSUZan6bX1VpQjQUUs8Am04d5xjJvmdzaxo72HQLQasbHIW0CQn/uWMLfG2hQwl6VS/a3xIgfo4jixPc5hnqpg3RG7NN+2GlUqMKMASQ0PvnchVe3NU8eCQKBgQC1mMd/JSqlDVAecuFZ6DWcKnecWm9rCWFKNRN7/0WzMiiku832Vz5hRD8Z2GrVb5S03k8zErhX8PShCZhHh+07xssU5GCtvHmik507es53N7+QEpErHAotYeuqmpPwk26vPftGqupgSR6v+yScfHbN2RjjqNwUeF1ggUQ8zZejVQKBgBYN1J//siK4AisP6L5yPyvCxpX+F3/uvXxThxdkDgxBJZdhpZb6YSRz5X26QKBP2iSp/eDaW6Awi5xQw6L2x/slUUKje/JXDzp6j185fD7m1UdVVb6rQ8EYY75+soFRi9xMatpSszpxzFn+oYRheZ5GnR/1qr0rU+EWPeF7eXMhAoGAXMoPDdGg46tUx+otabI0SnKMoZC4I7osQy2xogdCxxiXGe14hSn5Dtw/XeZuWFbmZjC9yTwEg7L0XKC5dbtnpcajGC7Fk6Cdikvg6HaMrvAGawiBssRmEuBY+dh4RVIgGV0TWv2UOtTB4Gv0Ph1+2xnWFOXAZplGJTyH8IWguO0CgYB83pByE7bEROLhuNc3DqIXD3RIjnhr/BKlM1ePQCsBj8MQL8qsgwDqYRE8SSUE30UMMQC+AdnJbhJ0OHSi2VHHAkd3kkXfTGryuWBsAckdHxXWQUAZOkTE3SlLc0kGm/mBUmXhbMEKk8yY3K9Tex0WLO3WXQWPY/00vGjIh1B8aw==";
     public static String ALIPAY_PUBLIC_KEY="MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAq0v1G5XmG71grmkRpmY8rac2rxpIZtPOAZcuBjsYtleo34jlsTqzjVU6Wif6VRGmyVluEno81KvxbDOcT/dKgmP7P/HW+EbmaenyWnvugAlI0KRFmIOPG5S8hFBkyyT8kG2smsSQ/ZEp0npCJrXYeMpVn0sAD2CJaGL3pumtgrwAk7r8LFg1r5bdbTvaeuHSXGucOAebgmoEad5bKQpq9i6NVI1GLqsgbCP9vgqKvHfGOvpLnxBzySzEn7ptKlMi9d0C7HCbReDXaTtM6inf+te0uv6qMgAiF9N5IhuFUTDtE80U9sE5BB5hl8nDGeARTDHNNWB6Ix6nnM8p+R8u7QIDAQAB";
     public static String sign_type="RSA2";
     public static String CHARSET="utf-8";
     public static String url = "https://openapi.alipay.com/gateway.do";
-    public static String NOTIFY_URL = "goku-yi.gnway.cc/alipay/notify_url.do";
-    public static String RETURN_URL = "goku-yi.gnway.cc/alipay/return_url.do";
+    public static String NOTIFY_URL = "http://www.modcreater.com/B/index.html/alipay/notify_url.do";
+    public static String RETURN_URL = "http://www.modcreater.com/B/index.html/return_url.do";
 
     AlipayClient alipayClient = new DefaultAlipayClient(url, APP_ID, APP_PRIVATE_KEY, "json", CHARSET, ALIPAY_PUBLIC_KEY,sign_type);
     AlipayTradeAppPayRequest request = new AlipayTradeAppPayRequest();
@@ -99,7 +100,7 @@ public class AliPayController {
         model.setBody("您花费"+userOrder.getPaymentAmount()+"元");
         model.setTimeoutExpress("30m");
         model.setProductCode("QUICK_MSECURITY_PAY");
-        request.setNotifyUrl("goku-yi.gnway.cc/alipay/notify_url.do");
+        request.setNotifyUrl(NOTIFY_URL);
         System.out.println(model.toString());
         request.setBizModel(model);
         try {
@@ -144,13 +145,13 @@ public class AliPayController {
         model.setBody("您花费"+userOrder.getPaymentAmount()+"元");
         model.setTimeoutExpress("30m");
         model.setProductCode("QUICK_MSECURITY_PAY");
-        request.setNotifyUrl("goku-yi.gnway.cc/alipay/notify_url.do");
+        request.setNotifyUrl(NOTIFY_URL);
         System.out.println(model.toString());
         request.setBizModel(model);
         try {
             //这里和普通的接口调用不同，使用的是sdkExecute
             AlipayTradeAppPayResponse response = alipayClient.sdkExecute(request);
-            String partner = APP_ID;
+            String partner = PID;
             String notify_url = NOTIFY_URL;
             String out_trade_no = userOrder.getId();
             String payment_type = "1";
@@ -181,7 +182,7 @@ public class AliPayController {
         return orderService.payInfoVerify(receivedVerifyInfo,request.getHeader("token"));
     }
 
-    @PostMapping(value = "/api/alipay/notify_url")
+    @PostMapping(value = "/alipay/notify_url")
     public String notify(HttpServletRequest request, HttpServletResponse response){
         System.out.println("调用了异步接口");
         Map<String, String> params = new HashMap<String, String>();
