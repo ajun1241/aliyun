ee9c00fc25541d4bf0fdc47bc1e768af7a95cfa8
Goku_yi <609274756@qq.com>
2019-07-19 10:05:59 +0800

    null

--- tmtrade/src/test/java/com/modcreater/tmtrade/TmtradeApplicationTests.java
+++ tmtrade/src/test/java/com/modcreater/tmtrade/TmtradeApplicationTests.java
@@ -1,13 +1,22 @@
 package com.modcreater.tmtrade;
 
+import com.modcreater.tmtrade.service.OrderService;
+import com.modcreater.tmtrade.service.impl.OrderServiceImpl;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.stereotype.Component;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.annotation.Resource;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@Component
 public class TmtradeApplicationTests {
+
+    @Resource
+    private OrderServiceImpl orderService;
 
     @Test
     public void contextLoads() {
