3eec8ef71300e6eef08b89cc93062b76d52dbc0a
Goku_yi <609274756@qq.com>
2019-09-23 16:41:40 +0800

    null

---
+++ tmbeans/src/main/java/com/modcreater/tmbeans/vo/eventvo/SeaByWeekForIOS.java
@@ -0,0 +1,24 @@
+package com.modcreater.tmbeans.vo.eventvo;
+
+import lombok.Data;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ *
+ * @Author: Goku_yi
+ * @Date: 2019-09-23
+ * Time: 16:32
+ */
+@Data
+public class SeaByWeekForIOS {
+
+    private String userId;
+
+    private String appType;
+
+    private String seaType;
+
+    private String friendId;
+
+}
