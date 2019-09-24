2c164c21013cfacdc24edb0e42c6f6fa8dc33d99
Goku_yi <609274756@qq.com>
2019-09-24 09:01:44 +0800

    null

---
+++ tmbeans/src/main/java/com/modcreater/tmbeans/pojo/StoreGoodsConsumable.java
@@ -0,0 +1,18 @@
+package com.modcreater.tmbeans.pojo;
+
+
+import lombok.Data;
+
+@Data
+public class StoreGoodsConsumable {
+
+  private long id;
+  private long goodsId;
+  private long consumableGoodsId;
+  private long registeredRatioIn;
+  private String registeredRationInUnit;
+  private long registeredRatioOut;
+  private String registeredRationOutUnit;
+  private long registeredTime;
+
+}
